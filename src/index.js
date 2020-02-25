69d3db8d573a1e17972f30ed395069fe6e28123f
John Medina <jhonandresmedina@gmail.com>
2020-02-24 23:12:53 -0500

    Initial Commit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
+import 'bootstrap/dist/css/bootstrap.css';
+import './index.scss';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+
 import * as serviceWorker from './serviceWorker';
+
+import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
