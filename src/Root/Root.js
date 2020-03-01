161d71727a6620f938cec8e87ec51d4831c73eee
John Medina <jhonandresmedina@gmail.com>
2020-02-29 19:17:56 -0500

    Setting component folders

--- src/Root/Root.js
+++ src/Root/Root.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
-import App from './App';
-import About from './About';
+import App from '../App';
+import About from '../About';
 
 const Root = () => {
   return (
